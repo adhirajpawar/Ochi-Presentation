70fa6dc40bb3d80ab271046249371d05d3441557
Adhiraj Pawar <adhirajpawar3@gmail.com>
2024-07-19 16:33:45 +0530

    done with eye effect

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,10 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Eyes() {
+    const [rotate, setRotate] = useState(0);
 
     useEffect(()=>{
          window.addEventListener("mousemove", (e)=>{
-            console.log(e)
+            let mouseX = e.clientX; 
+            let mouseY = e.clientY;
+
+            let deltaX = mouseX - window.innerWidth/2;
+            let deltaY = mouseY - window.innerHeight/2;
+
+            var angle = Math.atan2(deltaY, deltaX) * (180/Math.PI);
+            setRotate(angle-180);
          })
     })
 
@@ -14,15 +22,15 @@ function Eyes() {
         <div className='absolute flex gap-10 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'>
             <div className='flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-zinc-100'>
                 <div className='relative w-2/3 h-2/3 rounded-full bg-zinc-900'>
-                <div className='line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-10'>
-                    <div className=' w-7 h-7 rounded-full bg-zinc-100'></div>
+                <div style={{transform:`translate(-50%, -50%) rotate(${rotate}deg)`}} className='line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-10'>
+                    <div className='w-7 h-7 rounded-full bg-zinc-100'></div>
                 </div>
             </div>
         </div>
             <div className='flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-zinc-100'>
                 <div className='relative w-2/3 h-2/3 rounded-full bg-zinc-900'>
-                <div className='line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-10'>
-                    <div className=' w-7 h-7 rounded-full bg-zinc-100'></div>
+                <div style={{transform:`translate(-50%, -50%) rotate(${rotate}deg)`}} className='line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-10'>
+                    <div className='w-7 h-7 rounded-full bg-zinc-100'></div>
                 </div>
                 </div>
             </div>
