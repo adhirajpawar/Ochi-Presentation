a9b9321414ad81324ba5db27994db6128a010664
Adhiraj Pawar <adhirajpawar3@gmail.com>
2024-08-05 12:56:58 +0530

    added Features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -21,12 +21,25 @@ function Features() {
             </div>
 
         <div className="cards w-full flex gap-10 mt-5">
-            <div className="cardcontainer w-1/2 h-[70vh]">
-                <div className='card w-full h-full rounded-xl bg-green-500'></div>
+            <div className="cardcontainer relative w-1/2 h-[70vh]">
+            <h1 className='absolute text-[#CDEA68] left-full -translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] text-8xl font-semibold'>FYDE
+            <h1 className='absolute text-[#CDEA68] right-full translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] text-8xl font-semibold'>
+                {"FYDE".split('').map((item, index)=><span>{item}</span>)}
+            </h1>
+            </h1>
+
+                <div className='card w-full h-full rounded-xl overflow-hidden'>
+                    <img className="w-full h-full bg-cover" src="https://ochi.design/wp-content/uploads/2023/10/Fyde_Illustration_Crypto_2-663x551.png" alt="" />
+                </div>
             </div>
             
-            <div className="cardcontainer w-1/2 h-[70vh]">
-                <div className='card w-full h-full rounded-xl bg-green-500'></div>
+            <div className="cardcontainer relative w-1/2 h-[70vh]">
+            <h1 className='absolute text-[#CDEA68] right-full translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] text-8xl font-semibold'>VISE
+                {"VISE".split('').map((item, index)=><span>{item}</span>)}
+            </h1>
+                <div className='card w-full h-full rounded-xl overflow-hidden'>
+                    <img className="w-full h-full bg-cover" src="https://ochi.design/wp-content/uploads/2022/09/Vise_front2-663x551.jpg" alt="" />
+                </div>
             </div>
         </div>
         </div>
